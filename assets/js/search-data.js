23f047ed2c798ea3648a33c03e2ad4bace4203bb
jason9464 <47941842+jason9464@users.noreply.github.com>
2025-11-11 08:25:50 +0000

    Deploying to gh-pages from @ jason9464/jason9464.github.io@cbe7c93232ca5792b302479714d4c8cd73a35f2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,8 +384,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-page-facing-up-a-paper-has-been-accpeted-to-ijcai-2024",
-          title: ':page_facing_up: A paper has been accpeted to IJCAI 2024.',
+            },},{id: "news-page-facing-up-a-paper-has-been-accepeted-to-ijcai-2024",
+          title: ':page_facing_up: A paper has been accepeted to IJCAI 2024.',
           description: "",
           section: "News",},{id: "news-page-facing-up-a-paper-has-been-accepeted-to-icml-2024",
           title: ':page_facing_up: A paper has been accepeted to ICML 2024.',
