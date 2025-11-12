9cb213463fb4a98760df008c4a7f95f53017cddc
jason9464 <47941842+jason9464@users.noreply.github.com>
2025-11-12 01:29:22 +0000

    Deploying to gh-pages from @ jason9464/jason9464.github.io@a1981c1786231e68c20760a4e232107f8918aba6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,7 +456,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=OiDlGBwAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
