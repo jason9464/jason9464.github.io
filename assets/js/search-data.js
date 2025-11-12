417db041b0d638b3108d2ff7aab9dc6c6dc53446
jason9464 <47941842+jason9464@users.noreply.github.com>
2025-11-12 01:20:41 +0000

    Deploying to gh-pages from @ jason9464/jason9464.github.io@334bcf931e513d22c3852ce0c231a51fbca8108b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,21 +449,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%6A%73%68%65%6F%31%32%33%30%34@%70%6F%73%74%65%63%68.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -471,13 +457,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
